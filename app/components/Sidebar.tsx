6ee0f9cff2f3155b120cb71860c703ea600d92dc
Tien Dinh N. <tien.dinh@supremetech.vn>
2023-11-13 14:40:26 +0700

    Add sidebar

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import CalendarSmall from './CalendarSmall';
+
+const Sidebar = () => {
+  return (
+    <>
+      <CalendarSmall />
+    </>
+  );
+};
+
+export default Sidebar;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from '@remix-run/node';
 import CalendarContainer from '~/components/CalendarContainer';
+import Sidebar from '~/components/Sidebar';
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,8 +12,15 @@ export const meta: MetaFunction = () => {
 // ./app/routes/index.tsx
 export default function Index() {
   return (
-    <div className="">
-      <CalendarContainer />
+    <div className="page-home">
+      <div className="row">
+        <div className="col col-3">
+          <Sidebar />
+        </div>
+        <div className="col col-9">
+          <CalendarContainer />
+        </div>
+      </div>
     </div>
   );
 }
