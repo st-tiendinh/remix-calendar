df82800079db8a2a03d90465427a2b1f9cffb041
cmvn-thaile <thai.le@classmethod.vn>
2023-11-13 14:13:53 +0700

    setup sass

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,8 +10,8 @@ export const meta: MetaFunction = () => {
 // ./app/routes/index.tsx
 export default function Index() {
   return (
-    <div className="h-screen bg-slate-700 flex justify-center items-center">
-      <h2 className="text-blue-600 font-extrabold text-5xl">TailwindCSS Is Working!</h2>
+    <div className="">
+      <h2 className="text-danger">Sass Is Working!</h2>
     </div>
   )
 }
