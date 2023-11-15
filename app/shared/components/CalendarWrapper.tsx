c7182f8007e6f7103ef63caa873c4ed2b1e46f15
Tien Dinh N. <tien.dinh@supremetech.vn>
2023-11-15 10:27:58 +0700

    Create calendar component ui

--- app/shared/components/CalendarWrapper.tsx
+++ app/shared/components/CalendarWrapper.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
 import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 export default function CalendarWrapper() {
-  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
+  const handleSelect = (info: any) => {};
+  
+  return (
+    <div className="calendar-wrapper">
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+        dateClick={handleSelect}
+        headerToolbar={{
+          start: 'prev,today,next',
+          center: 'timeGridWeek,dayGridMonth,timeGridDay',
+          end: '',
+        }}
+        allDaySlot={false}
+        editable={true}
+        selectable={true}
+        select={handleSelect}
+        events={[
+          {
+            title: 'Play video game',
+            start: '2023-11-11T10:00:00',
+            end: '2023-11-11T12:00:00',
+            durationEditable: true
+          },
+        ]}
+      />
+    </div>
+  );
 }
