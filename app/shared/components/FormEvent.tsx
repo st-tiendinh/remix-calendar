939464154a0b1cb0764231595bc5b492a595df88
cmvn-thaile <thai.le@classmethod.vn>
2023-11-21 09:40:39 +0700

    modify error style

--- app/shared/components/FormEvent.tsx
+++ app/shared/components/FormEvent.tsx
@@ -6,12 +6,12 @@ import { EventData } from '../utils/types.server';
 export const eventSchema = z.object({
   title: z
     .string()
-    .min(1, { message: 'Title is required' })
-    .max(40, { message: 'Must be 40 or fewer characters long' }),
+    .min(1, { message: '*Title is required' })
+    .max(40, { message: '*Must be 40 or fewer characters long' }),
   description: z
     .string()
-    .min(1, { message: 'Description is required' })
-    .max(160, { message: 'Must be 160 or fewer characters long' }),
+    .min(1, { message: '*Description is required' })
+    .max(160, { message: '*Must be 160 or fewer characters long' }),
   date: z.coerce.date().refine(
     (data: Date) => {
       const currentDate = new Date();
@@ -19,18 +19,18 @@ export const eventSchema = z.object({
       return data >= currentDate;
     },
     {
-      message: 'Date must be in the future',
+      message: '*Date must be in the future',
     }
   ),
   timeStart: z.coerce
     .string()
-    .min(1, { message: 'Time start is required' })
+    .min(1, { message: '*Time start is required' })
     .max(24),
   timeEnd: z.coerce
     .string()
-    .min(1, { message: 'Time End is required' })
+    .min(1, { message: '*Time End is required' })
     .max(24),
-  location: z.string().min(1, { message: 'Location is required' }),
+  location: z.string().min(1, { message: '*Location is required' }),
   meetingLink: z.string().optional(),
 });
 
