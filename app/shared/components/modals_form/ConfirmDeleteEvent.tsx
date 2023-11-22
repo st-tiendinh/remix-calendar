99e0c2a55dc7d43c9c7f888d719f311d45ee4d27
cmvn-thaile <thai.le@classmethod.vn>
2023-11-22 17:23:17 +0700

    add ui for modal confirm

--- app/shared/components/modals_form/ConfirmDeleteEvent.tsx
+++ app/shared/components/modals_form/ConfirmDeleteEvent.tsx
@@ -7,18 +7,27 @@ const deleteEventSchema = z.object({});
 const ConfirmDeleteEvent = ({ eventId }: { eventId: string }) => {
   return (
     <>
-      <Form
-        schema={deleteEventSchema}
-        method="post"
-        action={`/events/${eventId}/delete`}
-      >
-        {({ Button }) => (
-          <>
-            <Button className="btn-delete">Yes</Button>
-            <Link to={`/events/${eventId}/edit`}>Cancel</Link>
-          </>
-        )}
-      </Form>
+      <div className="modal-confirm">
+        <p className="modal-confirm-title">
+          Are you sure you want to delete this event?
+        </p>
+        <Form
+          schema={deleteEventSchema}
+          method="post"
+          action={`/events/${eventId}/delete`}
+        >
+          {({ Button }) => (
+            <>
+              <div className="modal-confirm-footer">
+                <Button className="btn-confirm">Yes</Button>
+                <Link className="btn-cancel" to={`/events/${eventId}/edit`}>
+                  Cancel
+                </Link>
+              </div>
+            </>
+          )}
+        </Form>
+      </div>
     </>
   );
 };
