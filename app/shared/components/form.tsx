cdd812f7c4ff2513dd9d6c6a89bb104b51e727c1
Viet Nguyen T. <viet.nguyen@supremetech.vn>
2023-11-24 09:55:04 +0700

    change name file form

--- app/shared/components/form.tsx
+++ app/shared/components/form.tsx
@@ -1,7 +1,17 @@
-import { createForm } from 'remix-forms'
+import { createForm } from 'remix-forms';
 // For Remix, import it like this
-import { Form as FrameworkForm, useActionData, useSubmit, useNavigation } from '@remix-run/react'
+import {
+  Form as FrameworkForm,
+  useActionData,
+  useSubmit,
+  useNavigation,
+} from '@remix-run/react';
 
-const Form = createForm({ component: FrameworkForm, useNavigation, useSubmit, useActionData })
+const Form = createForm({
+  component: FrameworkForm,
+  useNavigation,
+  useSubmit,
+  useActionData,
+});
 
-export { Form }
+export { Form };
