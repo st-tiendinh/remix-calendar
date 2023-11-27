23f8385bfcf0a60ab5776ef38a932e362a9733ba
cmvn-thaile <thai.le@classmethod.vn>
2023-11-27 17:17:34 +0700

    update ui and toggle show hide sidebar

--- app/shared/components/Sidebar.tsx
+++ app/shared/components/Sidebar.tsx
@@ -1,49 +1,89 @@
 import { Link, Outlet, useLocation, useNavigation } from '@remix-run/react';
 
 import { type EventData } from '../utils/types.server';
-import EventList from './EventList';
 import { Spinner } from './Spinner';
+import logo from '../../../assets/images/logo.svg';
 
 interface SidebarProps {
   events: EventData[];
+  isShow: boolean;
+  setIsShow: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function Sidebar({ events }: SidebarProps) {
+export default function Sidebar({ events, isShow, setIsShow }: SidebarProps) {
   const location = useLocation();
   const navigation = useNavigation();
-  console.log(navigation.state);
 
   return (
-    <div className="col col-3 sidebar">
+    <>
       <div className="sidebar-header">
-        <button className="btn">
+        <button onClick={() => setIsShow((prev) => !prev)} className="btn">
           <i className="icon icon-list"></i>
         </button>
-        {location.pathname === '/events' ? (
-          <Link to={'/events/create'}>
-            <button className="btn">
-              <i className="icon icon-plus"></i>
-            </button>
-          </Link>
-        ) : (
-          <Link to={'/events'}>
-            <button className="btn">
-              <i className="icon icon-arrow-left"></i>
-            </button>
-          </Link>
-        )}
+        <Link className={`${isShow ? null : 'hide'}`} to="/">
+          <h1 className="logo">
+            <img className="logo-image" src={logo} alt="Fe Calendar logo" />
+          </h1>
+        </Link>
       </div>
       {navigation.state !== 'idle' ? (
         <Spinner />
       ) : (
         <>
           {location.pathname === '/events' ? (
-            <EventList events={events} />
+            // <EventList events={events} />
+            <>
+              <Link
+                className={`btn-create ${isShow ? '' : 'sm'} `}
+                to="/events/create"
+              >
+          
+                  <i className="icon icon-plus-circle"></i>
+        
+                <span className={`btn-create-text ${isShow ? null : 'hide'}`}>
+                  CREATE
+                </span>
+              </Link>
+
+              <div className={`today-event ${isShow ? '' : 'hide'}`}>
+                <div className="event-header">
+                  <h3 className="event-header-title">Today's Events:</h3>
+                  <span className="event-date">
+                    {new Date().toLocaleDateString()}
+                  </span>
+                </div>
+                <ul className="event-list">
+                  {events.map((event) => (
+                    <li key={event.title} className="event-item">
+                      <p>
+                        <i className="icon icon-active-event"></i>
+                      </p>
+                      <div className="event-detail">
+                        <div className="event-info">
+                          <span className="event-time">
+                            {event.timeStart} - {event.timeEnd}
+                          </span>
+                          {event.meetingLink && (
+                            <span className="icon-wrapper">
+                              <i className="icon icon-camera"></i>
+                            </span>
+                          )}
+                        </div>
+                        <h3 className="event-title">{event.title}</h3>
+                        <p className="event-meeting-link">
+                          {event.meetingLink}
+                        </p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </>
           ) : (
             <Outlet />
           )}
         </>
       )}
-    </div>
+    </>
   );
 }
