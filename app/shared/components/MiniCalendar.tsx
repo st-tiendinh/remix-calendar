c314f91c821d140e37c609d6a08628a89b67e76e
Viet Nguyen T. <viet.nguyen@supremetech.vn>
2023-11-29 09:38:22 +0700

    get api events for mini calendar

--- app/shared/components/MiniCalendar.tsx
+++ app/shared/components/MiniCalendar.tsx
@@ -1,8 +1,23 @@
-import { useNavigate } from '@remix-run/react';
+import { useLoaderData, useNavigate, useSearchParams } from '@remix-run/react';
+import { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
+import { EventData } from '../utils/types.server';
+import { LoaderFunction } from '@remix-run/node';
+
+export const loader: LoaderFunction = async ({}) => {
+  return null;
+};
 
 export default function MiniCalendar() {
+  const [params] = useSearchParams();
+  const [currentDateValue, setCurrentDateValue] = useState<Date>();
   const navigate = useNavigate();
+  const { eventsByMonth } = useLoaderData<typeof loader>();
+  useEffect(() => {
+    if (params.get('filter')) {
+      setCurrentDateValue(getDateParams());
+    }
+  }, [params]);
   const handleChangeDate = (date: Date, filterType: string) => {
     navigate(
       `/events?filter=${filterType}&day=${date.getDate()}&month=${
@@ -10,9 +25,35 @@ export default function MiniCalendar() {
       }&year=${date.getFullYear()}`
     );
   };
+  function isDateExists(inputDate: Date, events: EventData[]) {
+    const inputDateUTC = new Date(
+      inputDate.getTime() - inputDate.getTimezoneOffset() * 60000
+    );
+    const formattedInputDate = inputDateUTC.toISOString().split('T')[0];
+    const found = events.some((item) => {
+      const itemDate = new Date(item.date).toISOString().split('T')[0];
+      return itemDate === formattedInputDate;
+    });
+    return found;
+  }
+  const getDateParams = () => {
+    const day = params.get('day') ? params.get('day') : new Date().getDate();
+    const month = params.get('month')
+      ? params.get('month')
+      : new Date().getMonth() + 1;
+    const year = params.get('year')
+      ? params.get('year')
+      : new Date().getFullYear();
+
+    return new Date(`${year}-${month}-${day}`);
+  };
   return (
     <div className="mini-calendar">
       <Calendar
+        value={currentDateValue}
+        tileClassName={({ date }) => {
+          return isDateExists(date, eventsByMonth) ? 'active' : '';
+        }}
         onChange={(value: any) => handleChangeDate(value, 'day')}
         onClickMonth={(value) => handleChangeDate(value, 'month')}
       />
